88afa0a5430fac4bddf20de057739b1539d4469f
Mario Martinez <mariomtz2023@gmail.com>
2023-07-14 18:21:07 -0500

    Displayed first map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,4 +1,20 @@
-// Getting Started
+// Creating map
+let myMap = L.map('map', {
+    center: [29.4252, -98.4946],
+    zoom: 3
+});
 
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
-d3.json(url).then((data) => {console.log(data)});
+// Adding tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Link that holds GeoJSON data
+let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
+
+// Executing GeoJSON data
+d3.json(url).then((data) => {
+
+    console.log(data)
+
+});
