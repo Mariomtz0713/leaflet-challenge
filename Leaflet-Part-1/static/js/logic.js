b65ccb5d03f30b498926da99cc3142f5269404b1
Mariomtz0713 <mariomtz2023@gmail.com>
2023-07-20 11:46:43 -0500

    Complete

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -10,7 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(myMap);
 
 // Link that holds GeoJSON data
-let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
+let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
 // Executing GeoJSON data
 d3.json(url).then((data) => {
@@ -18,71 +18,73 @@ d3.json(url).then((data) => {
     // Promising data
     console.log(data)
     let features = data.features;
-    let metadata = data.metadata;
     
+    // Loop to extract and display
     for (let i = 0; i < features.length; i++) {
-        // Data query
+        
         let latitude = features[i].geometry.coordinates[1];
         let longitude = features[i].geometry.coordinates[0];
         let depth = features[i].geometry.coordinates[2];
 
-        // Determining marker color
+        // Determining marker color by depth
         if (depth >= -10 && depth < 10) {
-            depthColor = '#0000FF' // blue
+            depthColor = '#66ff66' // lime green
         }
 
         else if (depth >= 10 && depth < 30) {
-            depthColor = '#00FF00' // dark green
+            depthColor = '#ccff00' // light green
         }
 
         else if (depth >= 30 && depth < 50) {
-            depthColor = '#008000'  // light green
+            depthColor = '#ffff00'  // yellow
         }
 
         else if (depth >= 50 && depth < 70) {
-            depthColor = '#FFFF00' // yellow
+            depthColor = '#ffcc33' // lighter orange
         }
 
         else if (depth >= 70 && depth < 90) {
-            depthColor = '#FFA500' // orange
+            depthColor = '#FFA500' // dark orange
         }
 
         else {
             depthColor = '#FF0000' // red
         };
 
-        // Displaying marker and information
+        // Displaying marker
         let marker = L.circle([latitude, longitude], {
-            radius: features[i].properties.mag * 50000,
+            radius: features[i].properties.mag * 30000,
             color: 'black',
             weight: 0.5,
             fillColor: depthColor,
             fillOpacity: 0.75
         }).addTo(myMap);
 
+        // Displaying marker information
         marker.bindPopup(
         `Location: ${features[i].properties.place} <br>
-        Time: ${features[i].properties.time} <br>
-        Radius: ${features[i].properties.mag * 50000} <br>
+        Time: ${new Date(features[i].properties.time)} <br>
         Magnitude: ${features[i].properties.mag} <br>
-        Number of felt reports: ${features[i].properties.felt}`
+        Depth: ${depth}`
         );
     };
 
 });
 
+
+// Creating Legend
 let legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function() {
     let div = L.DomUtil.create('div', 'legend');
-    div.innerHTML += '<i style="background: #0000FF"></i>-10-10<br>'
-    div.innerHTML += '<i style="background: #008000"></i>10-30<br>'
-    div.innerHTML += '<i style="background: #00FF00"></i>30-50<br>'
-    div.innerHTML += '<i style="background: #FFFF00"></i>50-70<br>'
-    div.innerHTML += '<i style="background: #FFA500"></i>70-90<br>'
-    div.innerHTML += '<i style="background: #FF0000"></i>90+<br>'
+    div.innerHTML += '<l style="background: #66ff66"></l>-10-10<br>'
+    div.innerHTML += '<l style="background: #ccff00"></l>10-30<br>'
+    div.innerHTML += '<l style="background: #ffff00"></l>30-50<br>'
+    div.innerHTML += '<l style="background: #ffcc33"></l>50-70<br>'
+    div.innerHTML += '<l style="background: #FFA500"></l>70-90<br>'
+    div.innerHTML += '<l style="background: #FF0000"></l>90+<br>'
 
     return div;
 };
 
-legend.addTo(myMap)
+legend.addTo(myMap);
